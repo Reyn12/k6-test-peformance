3a1f561037b82cdb4f6596c6bcff60fe722de978
Reyn12 <muhammadrenaldi687@gmail.com>
2026-08-09 08:20:44 +0700

    ADJUST header

--- 05-api-post-test.js
+++ 05-api-post-test.js
@@ -26,7 +26,7 @@ export default function () {
   const params = {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${TOKEN}`,
+      'token': TOKEN,
     },
   };
 
